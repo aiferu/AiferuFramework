1f7aae4394dd34b7a11ec1a5bf4d036a3512542e
yx <1317044025@qq.com>
2023-05-31 12:32:47 +0800

    更新一下

--- Project/AiferuFramework/Assets/AiferuFramework/库本身的功能/4.项目使用工具收集/8.美术刷草工具/ArtBrushTool_Aiferu/Script/ArtBrushToolMain.cs
+++ Project/AiferuFramework/Assets/AiferuFramework/库本身的功能/4.项目使用工具收集/8.美术刷草工具/ArtBrushTool_Aiferu/Script/ArtBrushToolMain.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -8,17 +9,22 @@ namespace AiferuFramework.ArtBrushTool
     /// <summary>
     /// 美术刷草工具主类,用于调用美术刷草工具的功能,基础路径保存在这里
     /// </summary>
-    public class ArtBrushToolMain 
+    public class ArtBrushToolMain
     {
-        private readonly static string toolsPath;
-        private static string toolsDataSavePath;
-
-        public static string ToolsPath {
-            get {
-                return null;
+        public static string ToolsPath 
+        {
+            get 
+            {
+                return AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("ArtBrushTool_Aiferu")[0]);
             }
         }
-        public static string ToolsDataSavePath { get => toolsDataSavePath; set => toolsDataSavePath = value; }
+        public static string ToolsDataSavePath
+        {
+            get
+            {
+                return ToolsPath + "";
+;           }
+        }
     }
 }
 
